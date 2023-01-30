bcc7503859e9a66dfab5dec318bde68445e23582
willyyyyc <willcarmichaelcf@gmail.com>
2023-01-30 14:50:57 -0400

    Create Data point objects and add store in data_points list.

--- kMeans.py
+++ kMeans.py
@@ -5,6 +5,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
+class DataPoint:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
 #input n, the number of randomly generated points
 n = int(input("Enter an integer n to generate n points: "))
 r = int(input("Enter an integer r to set range of x and y where x,y E (-r, r): "))
@@ -17,9 +25,17 @@ while i <= n:
     y.append(random.randrange(-1 * r, r + 1))
     i+=1
 
-print(x)
-print(y)
+#print data points; a list of DataPoint objects containing an x and y (representing a coordinate on the plot)
+data_points = []
+i = 0
+while i <= n:
+    new_dp = DataPoint(x[i], y[i])
+    data_points.append(new_dp)
+    i += 1
 
+for dp in data_points:
+    print(dp)
+    
 fig, ax = plt.subplots()
 plt.scatter(x, y)
 plt.show()
